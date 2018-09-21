3d1fbe034ceab567ebd944ff4be602ca78288711
Lukas Tutkus <luksys5@yahoo.com>
2018-09-21 18:28:07 +0300

    VesCalc. Update packages with react-router-dom types, add SidebarButton component and its usage in App

--- VesiclesCalculations/src/components/App.tsx
+++ VesiclesCalculations/src/components/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
+import SidebarButton from './subComponents/SidebarButton';
 
 interface AppProps {
-
+  currentSectionName: string;
 }
 
 interface AppState {
-
 }
 
 class App extends React.Component<AppProps, AppState> {
+  private _homeSection = 'Home';
+  private _lipidVolumeSection = 'Lipid volume calculator'
+  private _molWeightSection = 'Molecular weight calculator'
+
   constructor(props) {
     super(props);
   }
@@ -18,23 +22,43 @@ class App extends React.Component<AppProps, AppState> {
   }
 
   public render(): JSX.Element {
+    const { currentSectionName } = this.props;
 
     return (
       <div className='app-container'>
-        <div className='flex-row row__non-middle'>
-          <div className='flex-column column__non-center'>a</div>
-          <div className='flex-column column__center'>b</div>
-          <div className='flex-column column__non-center'>c</div>
+        <div className='flex-row horizontaly-filled row__top'>
+          <div className='flex-column column__left'></div>
+          <div className='flex-column column__right'>
+            <div><h1>Vesicles Content Calculations</h1></div>
+            <div><h2>{ currentSectionName }</h2></div>
+          </div>
         </div>
-        <div className='flex-row row__middle'>
-          <div className='flex-column column__non-center'>a</div>
-          <div className='flex-column column__center'>b</div>
-          <div className='flex-column column__non-center'>c</div>
+        <div className='flex-row horizontaly-filled row__middle'>
+          <div className='flex-column column__left'>
+            <SidebarButton
+              className={ 'sidebar__button ' + (this._homeSection == currentSectionName ? 'active' : '') }
+              linkClassName='home-link'
+              linkTarget='/home'
+              label='Home'
+            />
+            <SidebarButton
+              className={ 'sidebar__button ' + (this._lipidVolumeSection == currentSectionName ? 'active' : '') }
+              linkClassName='lipid-vol-link'
+              linkTarget='/lipidVolume'
+              label='Lipid volume calculator'
+            />
+            <SidebarButton
+              className={ 'sidebar__button ' + (this._molWeightSection == currentSectionName ? 'active' : '') }
+              linkClassName='mol-weight-link'
+              linkTarget='/molWeight'
+              label='Molecular weight calculator'
+            />
+          </div>
+          <div className='flex-column column__right'>b</div>
         </div>
-        <div className='flex-row row__non-middle'>
-          <div className='flex-column column__non-center'>a</div>
-          <div className='flex-column column__center'>b</div>
-          <div className='flex-column column__non-center'>c</div>
+        <div className='flex-row horrizontaly-filled row__bottom'>
+          <div className='flex-column column__left'>a</div>
+          <div className='flex-column column__right'>b</div>
         </div>
       </div>
     );
