f18049ddb1c21e1ee28efa19dadb70c4c25d9507
Lukas Tutkus <luksys5@yahoo.com>
2019-02-05 14:01:06 +0200

    VesCalc. Update Pacakges, added style for loading

--- VesiclesCalculations/src/forms/components/LipidsMolWResult.tsx
+++ VesiclesCalculations/src/forms/components/LipidsMolWResult.tsx
@@ -38,14 +38,14 @@ export default class LipidsMolWResult extends React.Component<IMolWResultProps, 
         return (
         <div className='lipid-result extend--width'>
             <ResponsiveContainer className='chart-container' height='90%' width='100%'>
-            <LineChart data={ results.data } margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <LineChart data={ results.data } margin={{ top: 5, right: 30, left: 20, bottom: 5 }} isAnimationActive={false}>
                 <XAxis dataKey='diameter' />
                 <YAxis dataKey='lipidsTotalMolW'/>
                 <Tooltip />
                 <Legend />
                 <CartesianGrid strokeDasharray='3 3' />
-                { showLine !== ShowLine.molWeight && <Line name='Lipids count in vesicle' type='monotone' dataKey='lipidsInVesicle' stroke='black' /> }
-                { showLine !== ShowLine.lipidsCount && <Line name='Lipids molecular weight' type='monotone' dataKey='lipidsTotalMolW' stroke='#8884d8' /> }
+                { showLine !== ShowLine.molWeight && <Line name='Lipids count in vesicle' type='monotone' dataKey='lipidsInVesicle' stroke='black'  isAnimationActive={false}/> }
+                { showLine !== ShowLine.lipidsCount && <Line name='Lipids molecular weight' type='monotone' dataKey='lipidsTotalMolW' stroke='#8884d8'  isAnimationActive={false}/> }
             </LineChart>
             </ResponsiveContainer>
             <div className='result__selection'>
